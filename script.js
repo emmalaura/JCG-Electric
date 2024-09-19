75c17bba1e12ff85870916856d488be2a40687a8
emmalaura <emmalaura0422@gmail.com>
2024-09-18 22:31:19 -0500

    mobile responsiveness

--- script.js
+++ script.js
@@ -37,3 +37,13 @@ const navLinks = document.getElementById('nav-links');
 mobileMenu.addEventListener('click', () => {
     navLinks.classList.toggle('show');
 });
+
+
+
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.querySelector('.navbar');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navbar.classList.toggle('active');
+});
